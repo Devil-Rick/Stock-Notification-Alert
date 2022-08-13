d9a493a6d7768e1d909c2b159607d11854c1a730
Saptarshi <69756936+Devil-Rick@users.noreply.github.com>
2022-08-13 19:43:34 +0530

    sendin text message

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import config
 from newsapi import NewsApiClient
 from datetime import date
 from datetime import timedelta
+from twilio.rest import Client
 
-STOCK = "TSLA"
-COMPANY_NAME = "Tesla"
+STOCK_LIST = ["TSLA", "RKLB"]  # input all the req stock names
+COMPANY_LIST = ["Tesla", "Rocket Lab USA"]  # input all corresponding company names
+MSG_TEXT_LIST = []
 
 # ---------------------- Getting the date ------------------------#
 today = date.today()
@@ -13,37 +15,58 @@ yesterday = today - timedelta(days=1)
 day_before_yesterday = yesterday - timedelta(days=1)
 
 # ---------------------- Setting the Stock API ------------------------#
-market_url = "https://www.alphavantage.co/query"
-market_params = {
-    "function": "TIME_SERIES_DAILY",
-    "symbol": "RKLB",  # To check increase working change "RKLB"
-    "apikey": config.STOCK_API
-}
-market_response = req.get(market_url, params=market_params)
-market_response.raise_for_status()
-market_response_data = market_response.json()
-stk_price_current = float(market_response_data["Time Series (Daily)"][str(yesterday)]["4. close"])
-stk_price_previous = float(market_response_data["Time Series (Daily)"][str(day_before_yesterday)]["4. close"])
-percent_change = round(((stk_price_current - stk_price_previous) / stk_price_current) * 100, 3)
-if percent_change > 0:
-    print(f"{STOCK} : 🔺{abs(percent_change)}")
-elif percent_change < 0:
-    print(f"{STOCK} : 🔻{abs(percent_change)}")
+for i in range(len(STOCK_LIST)):
+    STOCK = STOCK_LIST[i]
+    COMPANY_NAME = COMPANY_LIST[i]
 
-# ---------------------- Setting the News API ------------------------#
-newsapi = NewsApiClient(api_key=config.NEWS_API)
-all_articles = newsapi.get_everything(q=COMPANY_NAME, domains="cnbc.com, financialexpress.com",
-                                      from_param='2022-08-01',
-                                      to='2022-08-12',
-                                      language='en',
-                                      sort_by='relevancy')
+    market_url = "https://www.alphavantage.co/query"
+    market_params = {
+        "function": "TIME_SERIES_DAILY",
+        "symbol": STOCK,
+        "apikey": config.STOCK_API
+    }
+    market_response = req.get(market_url, params=market_params)
+    market_response.raise_for_status()
+    market_response_data = market_response.json()
+    stk_price_current = float(market_response_data["Time Series (Daily)"][str(yesterday)]["4. close"])
+    stk_price_previous = float(market_response_data["Time Series (Daily)"][str(day_before_yesterday)]["4. close"])
+    percent_change = round(((stk_price_current - stk_price_previous) / stk_price_current) * 100, 3)
+    if percent_change > 0:
+        MSG_TEXT_LIST.append(f"{STOCK} : 🔺 {percent_change}")
+    elif percent_change < 0:
+        MSG_TEXT_LIST.append(f"{STOCK} : 🔻 {percent_change}")
 
-percent = (stk_price_previous * 5 / 100)
-if (stk_price_current >= stk_price_previous + percent) or (stk_price_current <= stk_price_previous - percent):
-    for i in range(3):
-        print(f'Headline: {all_articles["articles"][i]["title"]}')
-        print(f'Brief: {all_articles["articles"][i]["content"].partition("[")[0]}\n')
+    # ---------------------- Setting the News API ------------------------#
+    newsapi = NewsApiClient(api_key=config.NEWS_API)
+    all_articles = newsapi.get_everything(q=COMPANY_NAME,
+                                          domains="cnbc.com, financialexpress.com",
+                                          from_param=str(day_before_yesterday),
+                                          to=str(today),
+                                          language='en',
+                                          sort_by='relevancy')
 
-# ---------------------- Sending the message ------------------------#
-# TODO 1: STEP 3: Use https://www.twilio.com
-# Send a separate message with the percentage change and each article's title and description to your phone number.
+    percent = (stk_price_previous * 5 / 100)
+    if (stk_price_current >= stk_price_previous + percent) or (stk_price_current <= stk_price_previous - percent):
+        try:
+            for news in range(3):
+                MSG_TEXT_LIST.append(f'Headline: {all_articles["articles"][news]["title"]}')
+                MSG_TEXT_LIST.append(f'Brief: {all_articles["articles"][news]["content"].partition("+")[0][:-1]}\n')
+
+                # ---------------------- Sending the message ------------------------#
+                msg_text = "\n".join(MSG_TEXT_LIST)
+                client = Client(config.TWILIO_ID, config.TWILIO_API)
+                message = client.messages.create(
+                        body=msg_text,
+                        from_='+13149123008',
+                        to='+918016323773'
+                )
+                print(message.status)
+        except IndexError:
+            msg_text = "\n".join(MSG_TEXT_LIST)
+            client = Client(config.TWILIO_ID, config.TWILIO_API)
+            message = client.messages.create(
+                body=f"{msg_text}\nNo Breaking News Available for {COMPANY_NAME}",
+                from_='+13149123008',
+                to='+918016323773'
+            )
+            print(message.status)
